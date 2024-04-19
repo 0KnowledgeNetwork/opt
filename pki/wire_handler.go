09cd6fd6a2b99684ee1c4ba36630f321aa4e61ea
David Stainton <dstainton415@gmail.com>
2024-04-19 12:36:59 -0700

    pki: add wire handler

---
+++ pki/wire_handler.go
@@ -0,0 +1,236 @@
+package main
+
+import (
+	"crypto/ecdh"
+	"crypto/hmac"
+	"crypto/rand"
+	"net"
+	"time"
+
+	"github.com/katzenpost/hpqc/hash"
+	"github.com/katzenpost/hpqc/kem/schemes"
+
+	"github.com/katzenpost/katzenpost/core/cert"
+	"github.com/katzenpost/katzenpost/core/epochtime"
+	"github.com/katzenpost/katzenpost/core/pki"
+	"github.com/katzenpost/katzenpost/core/wire"
+	"github.com/katzenpost/katzenpost/core/wire/commands"
+)
+
+func (s *Server) onConn(conn net.Conn) {
+	const (
+		initialDeadline  = 30 * time.Second
+		responseDeadline = 60 * time.Second
+	)
+
+	rAddr := conn.RemoteAddr()
+	s.log.Debugf("Accepted new connection: %v", rAddr)
+
+	defer func() {
+		conn.Close()
+		s.wg.Done()
+	}()
+
+	// Initialize the wire protocol session.
+	auth := &wireAuthenticator{s: s}
+	keyHash := hash.Sum256From(s.identityPublicKey)
+
+	kemscheme := schemes.ByName(s.cfg.Server.WireKEMScheme)
+	if kemscheme == nil {
+		panic("kem scheme not found in registry")
+	}
+
+	cfg := &wire.SessionConfig{
+		KEMScheme:         kemscheme,
+		Geometry:          nil,
+		Authenticator:     auth,
+		AdditionalData:    keyHash[:],
+		AuthenticationKey: s.linkKey,
+		RandomReader:      rand.Reader,
+	}
+	wireConn, err := wire.NewPKISession(cfg, false)
+	if err != nil {
+		s.log.Debugf("Peer %v: Failed to initialize session: %v", rAddr, err)
+		return
+	}
+	defer wireConn.Close()
+
+	// Handshake.
+	conn.SetDeadline(time.Now().Add(initialDeadline))
+	if err = wireConn.Initialize(conn); err != nil {
+		s.log.Debugf("Peer %v: Failed session handshake: %v", rAddr, err)
+		return
+	}
+
+	// Receive a command.
+	cmd, err := wireConn.RecvCommand()
+	if err != nil {
+		s.log.Debugf("Peer %v: Failed to receive command: %v", rAddr, err)
+		return
+	}
+	conn.SetDeadline(time.Time{})
+
+	// Parse the command, and craft the response.
+	var resp commands.Command
+	if auth.isClient {
+		resp = s.onClient(rAddr, cmd)
+	} else if auth.isMix {
+		resp = s.onMix(rAddr, cmd, auth.peerIdentityKeyHash)
+	} else {
+		panic("wtf") // should only happen if there is a bug in wireAuthenticator
+	}
+
+	// Send the response, if any.
+	if resp != nil {
+		conn.SetDeadline(time.Now().Add(responseDeadline))
+		if err = wireConn.SendCommand(resp); err != nil {
+			s.log.Debugf("Peer %v: Failed to send response: %v", rAddr, err)
+		}
+	}
+}
+
+func (s *Server) onClient(rAddr net.Addr, cmd commands.Command) commands.Command {
+	s.log.Debug("onClient")
+	var resp commands.Command
+	switch c := cmd.(type) {
+	case *commands.GetConsensus:
+		resp = s.onGetConsensus(rAddr, c)
+	default:
+		s.log.Debugf("Peer %v: Invalid request: %T", rAddr, c)
+		return nil
+	}
+	return resp
+}
+
+func (s *Server) onMix(rAddr net.Addr, cmd commands.Command, peerIdentityKeyHash []byte) commands.Command {
+	s.log.Debug("onMix")
+	var resp commands.Command
+	switch c := cmd.(type) {
+	case *commands.GetConsensus:
+		resp = s.onGetConsensus(rAddr, c)
+	case *commands.PostDescriptor:
+		resp = s.onPostDescriptor(rAddr, c, peerIdentityKeyHash)
+	default:
+		s.log.Debugf("Peer %v: Invalid request: %T", rAddr, c)
+		return nil
+	}
+	return resp
+}
+
+func (s *Server) onGetConsensus(rAddr net.Addr, cmd *commands.GetConsensus) commands.Command {
+	resp := &commands.Consensus{}
+	resp.ErrorCode = commands.ConsensusOk
+
+	// FIXME: retrieve document from app chain / smart contract,
+	// cmd.Epoch will be used to indentify which doc to retrieve.
+	// resp.Payload = doc
+
+	return resp
+}
+
+func (s *Server) onPostDescriptor(rAddr net.Addr, cmd *commands.PostDescriptor, pubKeyHash []byte) commands.Command {
+	resp := &commands.PostDescriptorStatus{
+		ErrorCode: commands.DescriptorInvalid,
+	}
+
+	// Ensure the epoch is somewhat sane.
+	now, _, _ := epochtime.Now()
+	switch cmd.Epoch {
+	case now - 1, now, now + 1:
+		// Nodes will always publish the descriptor for the current epoch on
+		// launch, which may be off by one period, depending on how skewed
+		// the node's clock is and the current time.
+	default:
+		// The peer is publishing for an epoch that's invalid.
+		s.log.Errorf("Peer %v: Invalid descriptor epoch '%v'", rAddr, cmd.Epoch)
+		return resp
+	}
+
+	// Validate and deserialize the SignedUpload.
+	signedUpload := new(pki.SignedUpload)
+	err := signedUpload.Unmarshal(cmd.Payload)
+	if err != nil {
+		s.log.Errorf("Peer %v: Invalid descriptor: %v", rAddr, err)
+		return resp
+	}
+
+	desc := signedUpload.MixDescriptor
+
+	// Ensure that the descriptor is signed by the peer that is posting.
+	identityKeyHash := hash.Sum256(desc.IdentityKey)
+	if !hmac.Equal(identityKeyHash[:], pubKeyHash) {
+		s.log.Errorf("Peer %v: Identity key hash '%x' is not link key '%v'.", rAddr, hash.Sum256(desc.IdentityKey), pubKeyHash)
+		resp.ErrorCode = commands.DescriptorForbidden
+		return resp
+	}
+
+	descIdPubKey, err := cert.Scheme.UnmarshalBinaryPublicKey(desc.IdentityKey)
+	if err != nil {
+		s.log.Error("failed to unmarshal descriptor IdentityKey")
+		resp.ErrorCode = commands.DescriptorForbidden
+		return resp
+	}
+
+	if !signedUpload.Verify(descIdPubKey) {
+		s.log.Error("PostDescriptorStatus contained a SignedUpload with an invalid signature")
+		resp.ErrorCode = commands.DescriptorForbidden
+		return resp
+	}
+
+	// FIXME: Ensure that the descriptor is from an allowed peer.
+	/*
+		if !s.state.isDescriptorAuthorized(desc) {
+			s.log.Errorf("Peer %v: Identity key hash '%x' not authorized", rAddr, hash.Sum256(desc.IdentityKey))
+			resp.ErrorCode = commands.DescriptorForbidden
+			return resp
+		}
+	*/
+	// FIXME: send the mix descriptor to the app chain / smart contract.
+
+	// Return a successful response.
+	s.log.Debugf("Peer %v: Accepted descriptor for epoch %v: '%v'", rAddr, cmd.Epoch, desc)
+	resp.ErrorCode = commands.DescriptorOk
+	return resp
+}
+
+type wireAuthenticator struct {
+	s                   *Server
+	peerLinkKey         *ecdh.PublicKey
+	peerIdentityKeyHash []byte
+	isClient            bool
+	isMix               bool
+}
+
+func (a *wireAuthenticator) IsPeerValid(creds *wire.PeerCredentials) bool {
+	switch len(creds.AdditionalData) {
+	case 0:
+		a.isClient = true
+		return true
+	case hash.HashSize:
+	default:
+		a.s.log.Warning("Rejecting authentication, invalid AD size.")
+		return false
+	}
+
+	a.peerIdentityKeyHash = creds.AdditionalData
+
+	pk := [hash.HashSize]byte{}
+	copy(pk[:], creds.AdditionalData[:hash.HashSize])
+
+	// FIXME: query the app chain to determine if
+	// the identity is a mix or a provider.
+	isProvider := false
+	isMix := true
+	//_, isMix := a.s.state.authorizedMixes[pk]
+	//_, isProvider := a.s.state.authorizedProviders[pk]
+
+	if isMix || isProvider {
+		a.isMix = true // Providers and mixes are both mixes. :)
+		return true
+	} else {
+		a.s.log.Warning("Rejecting authority authentication, public key mismatch.")
+		return false
+	}
+
+	return false // Not reached.
+}
