7a39fa75609fb8e870c728c74f79cd80f122b759
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2024-07-16 17:30:44 -0700

    test(walletshield): add walletshield e2e tests and script (#31)

---
+++ tests/e2e/walletshield/test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+dir=$(dirname $(realpath "${0}"))
+uri=http://localhost:7070
+status=0
+
+run_test() {
+  test_dir="$1"
+  test_case="${test_dir#${dir}}"
+
+  if [ -f "${test_dir}/in.json" ]; then
+    curl \
+      -X POST \
+      -H 'Content-Type: application/json' \
+      -d "$(cat ${test_dir}/in.json)" \
+      --output /tmp/out.json \
+      --silent \
+      "${uri}${test_case}"
+
+    if ! diff "${test_dir}/out.json" /tmp/out.json > /dev/null; then
+      echo "${test_case}: ❌ failed"
+      status=1
+    else
+      echo "${test_case}: ✅ passed"
+    fi
+  else
+    echo "${test_case}: skipped (test data files not found)"
+  fi
+
+  sleep 1s
+}
+
+if [ -z "${1}" ]; then
+  for test_dir in $(find "${dir}" -mindepth 1 -type d)
+  do
+    run_test "${test_dir}"
+  done
+else
+  run_test "${dir}$1"
+fi
+
+exit ${status}
