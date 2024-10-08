e9ebb9c23f9b0db5d9f4159e20b9986fab54faa8
David Stainton <dstainton415@gmail.com>
2024-10-08 16:13:18 -0700

    feat: upgrade to katzenpost v0.0.38 and use client2
    - chore: upgrade to katzenpost v0.0.38
    - feat(walletshield): use client2
    - refactor(docker/Makefile): project specific targets, pass through others
    - refactor: replace customized genconfig with Makefile target
    - test: update test data output
    - ci: add walletshield test probe to workflow
    - feat(walletshield): add listen_client flag for client daemon network address
    - feat(walletshield): Add delayed start option
    
    ---------
    
    Co-authored-by: Xendarboh <1435589+xendarboh@users.noreply.github.com>

--- apps/walletshield/main.go
+++ apps/walletshield/main.go
@@ -18,23 +18,60 @@ import (
 	"github.com/charmbracelet/log"
 	"github.com/fxamacker/cbor/v2"
 
-	"github.com/katzenpost/katzenpost/client"
-	"github.com/katzenpost/katzenpost/client/config"
-	"github.com/katzenpost/katzenpost/client/utils"
+	"github.com/katzenpost/hpqc/hash"
+	"github.com/katzenpost/hpqc/rand"
+
+	"github.com/katzenpost/katzenpost/client2"
+	"github.com/katzenpost/katzenpost/client2/config"
+	"github.com/katzenpost/katzenpost/client2/thin"
+	sConstants "github.com/katzenpost/katzenpost/core/sphinx/constants"
 
 	"github.com/0KnowledgeNetwork/opt/server_plugins/cbor_plugins/http_proxy"
 )
 
+var (
+	timeout          = time.Second * 200
+	ProxyHTTPService = "http_proxy"
+)
+
+func sendRequest(thin *thin.ThinClient, payload []byte) ([]byte, error) {
+	surbID := &[sConstants.SURBIDLength]byte{}
+	_, err := rand.Reader.Read(surbID[:])
+	if err != nil {
+		panic(err)
+	}
+
+	// Select a target service node and compute the DestinationIdHash
+	target, err := thin.GetService(ProxyHTTPService)
+	if err != nil {
+		panic(err)
+	}
+	nodeId := hash.Sum256(target.MixDescriptor.IdentityKey)
+
+	timeoutCtx, _ := context.WithTimeout(context.TODO(), timeout)
+	return thin.BlockingSendMessage(timeoutCtx, payload, &nodeId, target.RecipientQueueID)
+}
+
+type Server struct {
+	log    *log.Logger
+	daemon *client2.Daemon
+	thin   *thin.ThinClient
+}
+
 func main() {
 	var logLevel string
 	var listenAddr string
+	var listenAddrClient string
 	var configPath string
+	var delayStart int
 	var testProbe bool
 	var testProbeCount int
 
 	flag.StringVar(&configPath, "config", "", "file path of the client configuration TOML file")
+	flag.IntVar(&delayStart, "delay_start", 0, "max random seconds to delay start")
 	flag.StringVar(&logLevel, "log_level", "DEBUG", "logging level could be set to: DEBUG, INFO, WARNING, ERROR, CRITICAL")
 	flag.StringVar(&listenAddr, "listen", "", "local socket to listen HTTP on")
+	flag.StringVar(&listenAddrClient, "listen_client", "", "local network address for the client daemon")
 	flag.BoolVar(&testProbe, "probe", false, "send test probes instead of handling requests")
 	flag.IntVar(&testProbeCount, "probe_count", 1, "number of test probes to send")
 	flag.Parse()
@@ -56,30 +93,44 @@ func main() {
 		Level:  level,
 	})
 
-	// mixnet client
+	if delayStart > 0 {
+		d := rand.NewMath().Intn(delayStart)
+		mylog.Infof("Delaying start for %d seconds...", d)
+		time.Sleep(time.Duration(d) * time.Second)
+	}
+
+	// start client2 daemon
 	cfg, err := config.LoadFile(configPath)
 	if err != nil {
 		panic(err)
 	}
-	c, err := client.New(cfg)
+
+	if listenAddrClient != "" {
+		cfg.ListenAddress = listenAddrClient
+	}
+
+	d, err := client2.NewDaemon(cfg)
 	if err != nil {
 		panic(err)
 	}
-	session, err := c.NewTOFUSession(context.Background())
+	err = d.Start()
 	if err != nil {
 		panic(err)
 	}
-	ProxyHTTPService := "http_proxy"
-	desc, err := session.GetService(ProxyHTTPService)
+
+	time.Sleep(time.Second * 3) // XXX ugly hack but works: FIXME
+
+	thin := thin.NewThinClient(cfg)
+	err = thin.Dial()
 	if err != nil {
 		panic(err)
 	}
 
 	// http server
 	server := &Server{
-		log:     mylog,
-		session: session,
-		target:  desc,
+		log:    mylog,
+		thin:   thin,
+		daemon: d,
 	}
 
 	if testProbe {
@@ -96,12 +147,6 @@ func main() {
 			}
 		}
 	}
-}
-
-type Server struct {
-	log     *log.Logger
-	session *client.Session
-	target  *utils.ServiceDescriptor
 }
 
 func (s *Server) Handler(w http.ResponseWriter, req *http.Request) {
@@ -128,7 +173,7 @@ func (s *Server) Handler(w http.ResponseWriter, req *http.Request) {
 		panic(err)
 	}
 
-	rawReply, err := s.session.BlockingSendUnreliableMessage(s.target.Name, s.target.Provider, blob)
+	rawReply, err := sendRequest(s.thin, blob)
 	if err != nil {
 		s.log.Errorf("Failed to send message: %s", err)
 		http.Error(w, "custom 404", http.StatusNotFound)
@@ -176,7 +221,8 @@ func (s *Server) SendTestProbes(d time.Duration, testProbeCount int) {
 	for {
 		packetsTransmitted++
 		t := time.Now()
-		_, err := s.session.BlockingSendUnreliableMessage(s.target.Name, s.target.Provider, blob)
+
+		_, err = sendRequest(s.thin, blob)
 		elapsed := time.Since(t).Seconds()
 		if err != nil {
 			s.log.Errorf("Probe failed after %.2fs: %s", elapsed, err)
@@ -189,13 +235,12 @@ func (s *Server) SendTestProbes(d time.Duration, testProbeCount int) {
 			if elapsed > rttMax {
 				rttMax = elapsed
 			}
-			s.log.Infof("Probe response took %.2fs", elapsed)
 		}
 
 		packetLoss := float64(packetsTransmitted-packetsReceived) / float64(packetsTransmitted) * 100
 		rttAvg := rttTotal / float64(packetsReceived)
-		s.log.Infof("Probe packet transmitted/received/loss = %d/%d/%.1f%%", packetsTransmitted, packetsReceived, packetLoss)
-		s.log.Infof("Probe rtt min/avg/max = %.2f/%.2f/%.2f s", rttMin, rttAvg, rttMax)
+		s.log.Infof("Probe packet transmitted/received/loss = %d/%d/%.1f%% | rtt min/avg/max = %.2f/%.2f/%.2f s",
+			packetsTransmitted, packetsReceived, packetLoss, rttMin, rttAvg, rttMax)
 
 		// probe indefinitely if testProbeCount is 0
 		if testProbeCount != 0 && packetsTransmitted >= testProbeCount {
--- go.mod
+++ go.mod
@@ -3,23 +3,25 @@ module github.com/0KnowledgeNetwork/opt
 go 1.22.3
 
 require (
-	github.com/BurntSushi/toml v1.3.2
+	github.com/BurntSushi/toml v1.4.0
 	github.com/carlmjohnson/versioninfo v0.22.5
 	github.com/charmbracelet/log v0.4.0
-	github.com/fxamacker/cbor/v2 v2.6.0
-	github.com/katzenpost/hpqc v0.0.35
-	github.com/katzenpost/katzenpost v0.0.35
+	github.com/fxamacker/cbor/v2 v2.7.0
+	github.com/katzenpost/hpqc v0.0.45
+	github.com/katzenpost/katzenpost v0.0.38
 	gopkg.in/op/go-logging.v1 v1.0.0-20160211212156-b2cb9fa56473
 )
 
 require (
-	codeberg.org/vula/highctidh v1.0.2024052900 // indirect
+	codeberg.org/vula/highctidh v1.0.2024092800 // indirect
 	filippo.io/edwards25519 v1.0.0 // indirect
 	filippo.io/mlkem768 v0.0.0-20240221181710-5ce91625fdc1 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/lipgloss v0.10.0 // indirect
 	github.com/go-faster/xor v1.0.0 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
+	github.com/google/pprof v0.0.0-20240903155634-a8630aee4ab9 // indirect
 	github.com/henrydcase/nobs v0.0.0-20230313231516-25b66236df73 // indirect
 	github.com/katzenpost/chacha20 v0.0.0-20190910113340-7ce890d6a556 // indirect
 	github.com/katzenpost/circl v1.3.9-0.20240222183521-1cd9a34e9a0c // indirect
@@ -34,20 +36,23 @@ require (
 	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/oasisprotocol/curve25519-voi v0.0.0-20230904125328-1f23a7beb09a // indirect
 	github.com/oasisprotocol/deoxysii v0.0.0-20220228165953-2091330c22b7 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/onsi/ginkgo/v2 v2.20.2 // indirect
+	github.com/quic-go/qpack v0.5.1 // indirect
+	github.com/quic-go/quic-go v0.47.0 // indirect
 	github.com/rfjakob/eme v1.1.2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
-	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
-	gitlab.com/elixxir/crypto v0.0.9 // indirect
-	gitlab.com/xx_network/crypto v0.0.6 // indirect
 	gitlab.com/yawning/aez.git v0.0.0-20211027044916-e49e68abd344 // indirect
 	gitlab.com/yawning/bsaes.git v0.0.0-20190805113838-0a714cd429ec // indirect
 	gitlab.com/yawning/x448.git v0.0.0-20221003101044-617eb9b7d9b7 // indirect
-	go.etcd.io/bbolt v1.3.8 // indirect
-	golang.org/x/crypto v0.21.0 // indirect
-	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
-	golang.org/x/net v0.23.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	go.etcd.io/bbolt v1.3.10 // indirect
+	go.uber.org/mock v0.4.0 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/exp v0.0.0-20240904232852-e7e105dedf7e // indirect
+	golang.org/x/mod v0.21.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	golang.org/x/tools v0.24.0 // indirect
 )
