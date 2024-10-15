011290a219dd0076b18b6ea152c2230bacedf2f2
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2024-10-14 17:34:59 -0700

    refactor(genconfig): remove docker and prometheus stuff

--- genconfig/main.go
+++ genconfig/main.go
@@ -476,7 +476,6 @@ func main() {
 	basePort := flag.Int("P", basePort, "First port number to use")
 	bindAddr := flag.String("a", bindAddr, "Address to bind to")
 	outDir := flag.String("o", "", "Path to write files to")
-	dockerImage := flag.String("d", "katzenpost-go_mod", "Docker image for compose-compose")
 	binSuffix := flag.String("S", "", "suffix for binaries in docker-compose.yml")
 	logLevel := flag.String("log_level", "DEBUG", "logging level could be set to: DEBUG, INFO, NOTICE, WARNING, ERROR, CRITICAL")
 	omitTopology := flag.Bool("D", false, "Dynamic topology (omit fixed topology definition)")
@@ -670,15 +669,6 @@ func main() {
 	if err != nil {
 		log.Fatalf("%s", err)
 	}
-
-	err = s.genDockerCompose(*dockerImage)
-	if err != nil {
-		log.Fatalf("%s", err)
-	}
-	err = s.genPrometheus()
-	if err != nil {
-		log.Fatalf("%s", err)
-	}
 }
 
 func identifier(cfg interface{}) string {
@@ -791,132 +781,4 @@ func cfgLinkKey(cfg interface{}, outDir string, kemScheme string) kem.PublicKey 
 		panic(err)
 	}
 	return linkPubKey
-}
-
-func (s *katzenpost) genPrometheus() error {
-	dest := filepath.Join(s.outDir, "prometheus.yml")
-	log.Printf("writing %s", dest)
-
-	f, err := os.Create(dest)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer f.Close()
-
-	write(f, `
-scrape_configs:
-- job_name: katzenpost
-  scrape_interval: 1s
-  static_configs:
-  - targets:
-`)
-
-	for _, cfg := range s.nodeConfigs {
-		write(f, `    - %s
-`, cfg.Server.MetricsAddress)
-	}
-	return nil
-}
-
-func (s *katzenpost) genDockerCompose(dockerImage string) error {
-	dest := filepath.Join(s.outDir, "docker-compose.yml")
-	log.Printf("writing %s", dest)
-	f, err := os.Create(dest)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer f.Close()
-
-	gateways, serviceNodes, mixes, err := s.genAuthorizedNodes()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	write(f, `version: "2"
-
-services:
-`)
-	for _, p := range gateways {
-		write(f, `
-  %s:
-    restart: "no"
-    image: %s
-    volumes:
-      - ./:%s
-    command: %s/server%s -f %s/%s/katzenpost.toml
-    network_mode: host
-
-    depends_on:`, p.Identifier, dockerImage, s.baseDir, s.baseDir, s.binSuffix, s.baseDir, p.Identifier)
-		for _, authCfg := range s.votingAuthConfigs {
-			write(f, `
-      - %s`, authCfg.Server.Identifier)
-		}
-	}
-
-	for _, p := range serviceNodes {
-		write(f, `
-  %s:
-    restart: "no"
-    image: %s
-    volumes:
-      - ./:%s
-    command: %s/server%s -f %s/%s/katzenpost.toml
-    network_mode: host
-
-    depends_on:`, p.Identifier, dockerImage, s.baseDir, s.baseDir, s.binSuffix, s.baseDir, p.Identifier)
-		for _, authCfg := range s.votingAuthConfigs {
-			write(f, `
-      - %s`, authCfg.Server.Identifier)
-		}
-	}
-
-	for i := range mixes {
-		// mixes in this form don't have their identifiers, because that isn't
-		// part of the consensus. if/when that is fixed this could use that
-		// identifier; instead it duplicates the definition of the name format
-		// here.
-		write(f, `
-  mix%d:
-    restart: "no"
-    image: %s
-    volumes:
-      - ./:%s
-    command: %s/server%s -f %s/mix%d/katzenpost.toml
-    network_mode: host
-    depends_on:`, i+1, dockerImage, s.baseDir, s.baseDir, s.binSuffix, s.baseDir, i+1)
-		for _, authCfg := range s.votingAuthConfigs {
-			// is this depends_on stuff actually necessary?
-			// there was a bit more of it before this function was regenerating docker-compose.yaml...
-			write(f, `
-      - %s`, authCfg.Server.Identifier)
-		}
-	}
-	for _, authCfg := range s.votingAuthConfigs {
-		write(f, `
-  %s:
-    restart: "no"
-    image: %s
-    volumes:
-      - ./:%s
-    command: %s/voting%s -f %s/%s/authority.toml
-    network_mode: host
-`, authCfg.Server.Identifier, dockerImage, s.baseDir, s.baseDir, s.binSuffix, s.baseDir, authCfg.Server.Identifier)
-	}
-
-	write(f, `
-  %s:
-    restart: "no"
-    image: %s
-    volumes:
-      - ./:%s
-    command: --config.file="%s/prometheus.yml"
-    network_mode: host
-`, "metrics", "docker.io/prom/prometheus", s.baseDir, s.baseDir)
-
-	return nil
 }
